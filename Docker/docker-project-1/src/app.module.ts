a57e211916be7a7d9b8f47658d180e1143c5f9b5
YeomJaeSeon <a89541457@gmail.com>
2022-06-21 23:03:26 +0900

    도커 실습 1

---
+++ Docker/docker-project-1/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserEntity } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'kingbobne',
+      entities: [UserEntity],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
