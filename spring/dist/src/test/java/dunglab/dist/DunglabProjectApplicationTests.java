8e0a0fd60100d19925e6b4933169188d2d2816e2
YeomJaeSeon <a89541457@gmail.com>
2022-09-28 23:56:33 +0900

    spring mvc ~chapter2

---
+++ spring/dist/src/test/java/dunglab/dist/DunglabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dunglab.dist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunglabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
