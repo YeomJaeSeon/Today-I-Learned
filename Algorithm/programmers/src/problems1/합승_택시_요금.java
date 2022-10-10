78e695758c57a28860be35b557ae59a1fe2c519b
YeomJaeSeon <a89541457@gmail.com>
2022-10-10 18:36:14 +0900

    solve 2606 - bfs

---
+++ Algorithm/programmers/src/problems1/합승_택시_요금.java
@@ -0,0 +1,12 @@
+package problems1;
+
+public class 합승_택시_요금 {
+    public static void main(String[] args) {
+        합승_택시_요금 instance = new 합승_택시_요금();
+        System.out.println(instance.solution(6, 4, 6, 2, new int[][]{{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}}));
+    }
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
+        return answer;
+    }
+}
